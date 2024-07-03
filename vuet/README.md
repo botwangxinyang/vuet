106ff8bf249502c7a8c39a804ff38f80e138f60f
unknown <xinyang3@andrew.cmu.edu>
2024-07-03 18:33:33 +0800

    add the user login page

--- vuet/README.md
+++ vuet/README.md
@@ -1,4 +1,4 @@
-# hello-world
+# vuet
 
 ## Project setup
 ```
---
+++ vuet/src/components/UserLogin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login-container">
+        <h1>Login</h1>
+        <el-input placeholder="请输入用户名" id="UserName"></el-input>
+        <el-input placeholder="请输入密码" id="Password"></el-input>
+        <el-button type="primary" round></el-button>
+    </div>
+    
+</template>
+<style scoped>
+.login-container {
+    max-width: 300px;
+    margin: 50px auto;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  }
+</style>
